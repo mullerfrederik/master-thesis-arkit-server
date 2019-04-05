6962f5560b494fb19a78847fad307eb25ca3c4ca
Frederik Muller <frederik.muller@icloud.com>
2019-04-05 21:34:21 +0200

    asfasf

--- Package.swift
+++ Package.swift
@@ -1,16 +1,22 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "VaporApp",
-    dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
+    name: "Server",
+    products: [
+        .library(name: "Server", targets: ["App"]),
     ],
-    exclude: [
-        "Config",
-        "Database",
-        "Localization",
-        "Public",
-        "Resources",
-        "Tests",
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+
+        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"])
     ]
 )
+
